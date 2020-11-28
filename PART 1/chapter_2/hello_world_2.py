71a0b8dd91c7f779d721000d18f7688b5584caf8
Vien Ngo <zenvin@protonmail.com>
2020-11-27 21:42:30 -0800

    Chapter_1
    Completed chapter 1 and started chapter 2

---
+++ PART 1/chapter_2/hello_world_2.py
@@ -0,0 +1,36 @@
+{
+	"cmd": ["python3", "-u" "$file"],
+}
+
+#creating a variable
+message = "Hello Python World!"
+print(message)
+
+#creating the variable again to see what will be printed
+message = "Hello Python Crash Course world!"
+print(message)
+
+#the message variable is updated and will print the second message.
+#You can change the value of a variable in your program at any time, and Python will always keep track of its current value.
+
+# Variable rules: can only contain letters, numbers and underscore. Can start with a letter or underscore, but not a number. 
+# Spaces are not allowed in variable names.
+# Avoid using Python keywords and function in a variable name.
+# Variable names should be short by descriptive.
+# Be careful with the lower case letter l and uppercase O, as they can be confused with 1 and 0.
+# Use lowercase for now.
+
+#Variables are labels.
+
+#TRY IT YOURSELF
+
+# Assign a message to a variable, and then print that message
+motivational_message = "Don't quit!"
+print(motivational_message)
+
+motivational_message = "You can rest, but don't you quit!"
+print(motivational_message)
+
+
+
+
---
+++ PART 1/chapter_2/name.py
@@ -0,0 +1,38 @@
+name = "ada lovelace"
+print(name.title())
+
+# title() is a method. A method is an action that Python can perform on a piece of data (in this case, proper case).
+# the . after name, tells Python to make the title() method act on the variable name.
+
+print(name.upper())
+print(name.lower())
+
+# Sometimes we will want to use the variable value inside a string. For example, we might want two variables to represent a first name and a last name:
+# f-strings is shown below where the f and the the "" is used with the variables placed between it.
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+#You can also use f-string:
+print(f"Hello, {full_name.title()}")
+
+#You can also use f-strings to compose a message and then assign the entire message to a variable:
+message = f"Hello, {full_name.title()}"
+print(message)
+
+# the use of a variable and then printing the variable makes the print() call much simpler.
+# note: f-strings were introduced in Python 3.6.  In 3.5 and earlier editions, one would use format().
+# example: full_name = "{} {}".format(first_name, last_name)
+
+#Adding whitespace to strings. Whitespace refers to any nonprinting character, such as spaces, tabs and end-of-line symbols. 
+#TAB
+print("Python")
+print("\tPython")
+#NEWLINE
+print("Languages:n\nPython\nC\nJavaScript")
+#COMBINE TABS and NEW LINE
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+
+#END ON PAGE 22 
